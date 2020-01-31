4a9507f7ad28d0c520488e5d600beb9f841e217c
Jacob Gough <jacob.gough@amplyfigroup.com>
2020-01-31 13:16:35 +0000

    fix null map error

--- src/services/video_service.ts
+++ src/services/video_service.ts
@@ -1,24 +1,26 @@
 import Video, { BlankVideo } from "../models/video";
 import axios, { AxiosResponse } from "axios";
 
-const apiPath: string = "/blogs";
+const apiPath: string = "";
 
 export async function getAllVideos(): Promise<Video[]> {
-  try {
-    const response: AxiosResponse<Video[]> = await axios.get<Video[]>(apiPath);
-    return response.data;
-  } catch (e) {
-    console.log(e);
-    return [];
-  }
+  // try {
+  //   const response: AxiosResponse<Video[]> = await axios.get<Video[]>(apiPath);
+  //   return response.data;
+  // } catch (e) {
+  //   console.error(e);
+  //   return [];
+  // }
+  return [];
 }
 
 export async function getVideo(id: string): Promise<Video> {
-  try {
-    const response: AxiosResponse<Video> = await axios.get<Video>(`${apiPath}/${id}`);
-    return response.data;
-  } catch (e) {
-    console.log(e);
-    return BlankVideo();
-  }
+  // try {
+  //   const response: AxiosResponse<Video> = await axios.get<Video>(`${apiPath}/${id}`);
+  //   return response.data;
+  // } catch (e) {
+  //   console.error(e);
+  //   return BlankVideo();
+  // }
+  return BlankVideo();
 }
